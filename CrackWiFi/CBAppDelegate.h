9de817f0b0c96af05d4ca1580697eed4f9b652d0
liu464698104 <meijust@sina.cn>
2014-07-09 11:55:41 +0800

    Initial Commit

---
+++ CrackWiFi/CBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CBAppDelegate.h
+//  CrackWiFi
+//
+//  Created by 时代合盛 on 14-7-9.
+//  Copyright (c) 2014年 时代合盛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrackWiFi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrackWiFi
+//
+//  Created by 时代合盛 on 14-7-9.
+//  Copyright (c) 2014年 时代合盛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CBAppDelegate class]));
+    }
+}
